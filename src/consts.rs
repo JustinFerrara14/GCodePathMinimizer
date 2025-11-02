7a7815fbb0b8085c1a3138166b2c825c7baff985
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2025-11-02 21:14:20 +0100

    feat: add kd-tree

--- src/consts.rs
+++ src/consts.rs
@@ -1,2 +1,5 @@
 pub const GCODE_FOLDER: &str = "objets3D";
 pub const PRINTING_SPEED_MM_PER_SEC: u64 = 60;
+
+pub const NUMBER_OF_NEARESTS_TO_CHECK: usize = 3;
+pub const KDTREE_BUCKET_SIZE: usize = 128;   // default is 32
--- src/minimizer.rs
+++ src/minimizer.rs
@@ -1,5 +1,12 @@
 use indicatif::{ProgressBar, ProgressStyle};
+use std::collections::HashSet;
+// use kiddo::KdTree;
+use kiddo::float::kdtree::KdTree;
+use kiddo::SquaredEuclidean;
+use kiddo::NearestNeighbour;
+
 use crate::utils::*;
+use crate::consts::*;
 
 pub fn minimize_gcode(gcode: &mut GCodeData) -> () {
 
@@ -9,10 +16,8 @@ pub fn minimize_gcode(gcode: &mut GCodeData) -> () {
     // For each layer
     for i in 0..gcode.layers.len() {
 
-        // Check if it is the first layer, use the first segment as default
-        if i == 0 {
-            // Do nothing for the first layer
-        } else {
+        // Skip the first layer (it will start from the first segment)
+        if i > 0 {
             // For other layers, the first segment is the one closest to the last segment of the previous layer
             let previous_layer = &gcode.layers[i - 1];
             if let Some(last_segment_prev_layer) = previous_layer.segments.last() {
@@ -37,38 +42,61 @@ pub fn minimize_gcode(gcode: &mut GCodeData) -> () {
         let mut ordered_segments = Vec::new();
         if let Some(first_segment) = gcode.layers[i].segments.first() {
 
-            // Add the first segment to the ordered list
             ordered_segments.push(first_segment.clone());
             let mut remaining_segments: Vec<Segment> = gcode.layers[i].segments[1..].to_vec();
+            let mut remaining_indices: HashSet<usize> = (0..remaining_segments.len()).collect();
 
-            while !remaining_segments.is_empty() {
+            // Create kd-tree for the segments in the layer
+            let mut kdtree: KdTree<_, _, 2, KDTREE_BUCKET_SIZE, u32> = KdTree::with_capacity(remaining_segments.len() * 2);
+
+            for (i, s) in remaining_segments.iter().enumerate() {
+                kdtree.add(&[s.x1 as f64, s.y1 as f64], i as u64);
+                kdtree.add(&[s.x2 as f64, s.y2 as f64], i as u64);
+            }
+
+            let mut number_of_nearests_to_check = NUMBER_OF_NEARESTS_TO_CHECK;
+            while !remaining_indices.is_empty() {
                 let last_segment = ordered_segments.last().unwrap();
-                let mut closest_segment_index = 0;
-                let mut closest_segment_reversed = false;
-                let mut closest_distance = std::f64::MAX;
+                let last_point = [last_segment.x2 as f64, last_segment.y2 as f64];
 
-                for (i, segment) in remaining_segments.iter().enumerate() {
-                    let distance = last_segment.length_with_other(segment);
-                    let distance_reverse = last_segment.length_with_other(&segment.reversed());
+                // Find the nearest neighbor in the kd-tree
+                let nearests = kdtree.nearest_n::<SquaredEuclidean>(&last_point, number_of_nearests_to_check);
 
-                    let min_distance = distance.min(distance_reverse);
 
-                    if min_distance < closest_distance {
-                        closest_distance = min_distance;
-                        closest_segment_index = i;
-                        if distance_reverse < distance {
-                            closest_segment_reversed = true;
+                // Check if already used
+                for i in 0..nearests.len() {
+                    let nearest = &nearests[i];
+                    let nearest_index = nearest.item as usize;
+                    if remaining_indices.contains(&nearest_index) {
+                        // Found the nearest unused segment
+                        let mut next_segment = remaining_segments[nearest_index].clone();
+
+                        // Determine if we need to reverse the segment
+                        if last_segment.length_with_other(&next_segment.reversed()) < last_segment.length_with_other(&next_segment) {
+                            next_segment = next_segment.reversed();
                         }
+
+                        // Add the closest segment to the ordered list
+                        ordered_segments.push(next_segment.clone());
+                        remaining_indices.remove(&nearest_index);
+
+                        // Remove the segment from remaining and kd-tree
+                        kdtree.remove(&[next_segment.x1 as f64, next_segment.y1 as f64], nearest.item);
+                        kdtree.remove(&[next_segment.x2 as f64, next_segment.y2 as f64], nearest.item);
+                        break;
+                    } else {
+                        // Check if this was the last nearest to check
+                        if i == nearests.len() - 1 {
+                            // All nearests are used, need to increase the number to check
+                            number_of_nearests_to_check += 3;
+                        }
+
+                        // Continue to next nearest
+                        continue;
                     }
                 }
-
-                // Add the closest segment to the ordered list and remove it from remaining
-                let mut next_segment = remaining_segments.remove(closest_segment_index);
-                if closest_segment_reversed {
-                    next_segment = next_segment.reversed();
-                }
-                ordered_segments.push(next_segment);
             }
+
         } else {
             // Layer has no segments, do nothing
         }
